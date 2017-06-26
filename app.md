ce296b20f2f224cf0c55ead2cdf936d4c0816b09
leonardust <luk.komorowski@gmail.com>
2017-06-26 22:28:22 +0200

    update

--- app.md
+++ app.md
@@ -5,7 +5,7 @@ http://diabcontrol3.herokuapp.com
 
 ## usernames 
 admin
-doctor_i@example.com
+doctor_a@example.com
 doctor_b@example.com
 
 ## password 
--- examples/list_comp.py
+++ examples/list_comp.py
@@ -1,20 +1,26 @@
 #!/usr/bin/python
 
+
 class Patient():
     def __init__(self, name, surname):
         self.name = name
         self.surname = surname
 
-if __name__ == '__main__':
-    patients = []
-    patients.append(Patient('John', 'Doe'))
-    patients.append(Patient('Jan', 'Kowalski'))
-    
-    # latwiej, szybciej
-    print([patient.name for patient in patients])
 
-    # standardowo
+if __name__ == '__main__':
+    patients = [
+        Patient('John', 'Doe'),
+        Patient('Jan', 'Kowalski')
+    ]
+
+    # don't say anything to me
+    print(patients)
+
+    # classic way
     result = []
     for patient in patients:
         result.append(patient.name)
-    print result
+    print(result)
+
+    # list comprehension
+    print([patient.name for patient in patients])
